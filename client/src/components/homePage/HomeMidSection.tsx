d640d13891ebf45b5d2dba8855ffc0ed927a28fd
jswyndham <kyotojim2@gmail.com>
2024-08-11 13:25:15 +0900

    Add scroll to the top of the page 7

--- client/src/components/homePage/HomeMidSection.tsx
+++ client/src/components/homePage/HomeMidSection.tsx
@@ -1,15 +1,22 @@
+import { useEffect } from 'react';
 import HeadingOne from '../HeadingOne';
-import ScrollToTop from '../ScrollToTop';
 import MenuCards from './MenuCards';
+import { useLocation } from 'react-router-dom';
 
 function HomeMidSection() {
+	const location = useLocation();
+
+	useEffect(() => {
+		if (location.pathname.includes('/menu')) {
+			window.scrollTo(0, 0);
+		}
+	}, [location.pathname]);
+
 	return (
 		<article className="w-full h-fit flex justify-center">
 			<div className="w-10/12 flex flex-col text-center">
 				<HeadingOne headingOneText={"Porto Bello's Food Menu"} />
-
 				<div className="px-2 py-8 lg:p-12 2xl:px-32 2xl:py-16">
-					<ScrollToTop />
 					<MenuCards />
 				</div>
 			</div>
