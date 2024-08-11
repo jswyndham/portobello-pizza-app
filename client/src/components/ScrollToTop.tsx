961579e1089fcdceec92b35e0237041820aeb84f
jswyndham <kyotojim2@gmail.com>
2024-08-11 13:13:52 +0900

    Add scroll to the top of the page 4

--- client/src/components/ScrollToTop.tsx
+++ client/src/components/ScrollToTop.tsx
@@ -2,11 +2,11 @@ import { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 
 const ScrollToTop = () => {
-	const { pathname } = useLocation(); // set the path location
+	const { pathname, search, hash } = useLocation(); // set the path location
 
 	useEffect(() => {
 		window.scrollTo(0, 0); // Scroll the window to the top
-	}, [pathname]); // Effect runs every time the pathname changes
+	}, [pathname, search, hash]); // Effect runs every time the pathname, search, or hash tag changes
 
 	return null; // Component doesn't render anything in the UI
 };
